e635a2d788bbec30a9c8107f589f8e8dfa90f284
deepak-jacob <jacob.deepak@yahoo.com>
2016-11-23 22:39:13 +0000

    Why asynclip

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const async = require("async");
-console.log(typeof async);
+const fs = require("fs");
+
+async.map(["index.js", "package.json", "README.md"], fs.stat, function(err, res) {
+	console.log(res);
+});
+
+//without async
+fs.stat("package.json", function(err, stats) {
+	console.log(stats);
+});
