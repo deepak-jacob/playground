06f0c01c7dd1087f90d729b018bef3d44a47a00b
Deepak Jacob <jacob.deepak@yahoo.com>
2017-01-01 09:04:55 +0530

    Install nvm

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt-get install chromium-browser
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt update
 sudo apt install atom
-
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
