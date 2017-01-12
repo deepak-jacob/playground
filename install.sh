9c9ae835ce446f306b3891212f300eb0619838ba
Deepak Jacob <jacob.deepak@yahoo.com>
2017-01-12 23:53:16 +0530

    Use apt

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ sudo apt-get -f install
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt install google-chrome-stable
 sudo apt install vlc
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo apt-get update
