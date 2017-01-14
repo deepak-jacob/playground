6f624a908047d55c3531894e7d4cbf4ccd3fdcce
Deepak Jacob <jacob.deepak@yahoo.com>
2017-01-14 22:05:16 +0530

    Use apt

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo dpkg -i teamviewer_i386.deb
 sudo apt-get -f install
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update
+sudo apt update
 sudo apt install google-chrome-stable
 sudo apt install vlc
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
